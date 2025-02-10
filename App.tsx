75dfa3c7302498b8be18aeaf27b060d658beb7ec
MihajaFi <hei.fifaliana@gmail.com>
2025-02-10 22:54:42 +0300

    feat: request notification permission and fetch FCM token

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.text}>This is a simple app</Text>
+      <Text style={styles.text}>That can receive notifications from Firebase</Text>
     </View>
   );
 }
@@ -14,12 +16,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f4f8', // Light background
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+    textTransform: 'uppercase',
+    letterSpacing: 1.5,
   },
   text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#000',
+    fontSize: 18,
+    color: '#555',
+    textAlign: 'center',
+    marginVertical: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 3, // Android shadow effect
   },
 });
 
